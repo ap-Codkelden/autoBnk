c5308e53d1838be70d47ec8c4600f8f9bebb8852
Rino <mavladi@gmail.com>
2013-05-11 21:29:00 +0300

    Delimeters added

--- autobnk4.py
+++ autobnk4.py
@@ -51,20 +51,20 @@ class db_processing:
 	def cross_process(self):
 		self.db_cur.execute("ALTER TABLE etalon RENAME TO temp_names")
 		self.db_cur.execute("CREATE TABLE bank_sum (name text, raj83 integer, raj87 integer, raj18 integer)")
-		self.db_cur.execute("INSERT INTO bank_sum 
-								SELECT u.name, 
-										s83.zn as raj83, 
-										s87.zn as raj87, 
-										s18.zn as raj18 
-								from temp_names u 
-									left outer join 
-										itog s83 on u.code = s83.code 
-										and s83.raj = 83 
-									left outer join 
-										itog s87 on u.code = s87.code 
-										and s87.raj = 87 
-									left outer join 
-										itog s18 on u.code = s18.code 
+		self.db_cur.execute("INSERT INTO bank_sum \
+								SELECT u.name, \
+										s83.zn as raj83, \
+										s87.zn as raj87, \
+										s18.zn as raj18 \
+								from temp_names u \
+									left outer join \
+										itog s83 on u.code = s83.code \
+										and s83.raj = 83 \
+									left outer join \
+										itog s87 on u.code = s87.code \
+										and s87.raj = 87 \
+									left outer join \
+										itog s18 on u.code = s18.code \
 										and s18.raj = 18")
 
 	def make_etalon(self):
@@ -78,9 +78,9 @@ class db_processing:
 	def create_tables(self, raj_list):
 		# создание таблиц
 		self.db_cur.execute("CREATE TABLE bank (raj integer, rozd text, rd text, pg text, st text, zn integer, bd integer)")
-		self.engine.commit()
+		#self.engine.commit()
 		self.db_cur.execute("CREATE TABLE itog (code text, raj integer, zn integer)")
-		self.engine.commit()
+		#self.engine.commit()
 		self.db_cur.execute("CREATE TABLE etalon (code text, name text)")
 		self.engine.commit()
 
